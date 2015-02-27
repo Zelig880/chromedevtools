80410910e74f58f461baf8fafbcd7311e0cd6da3
msoWebDesign <scuomo1988@gmail.com>
2015-02-27 18:55:31 +0000

    include regular expression to check for special characters used into the request

--- src/index.php
+++ src/index.php
@@ -2,6 +2,8 @@
 <?php 
 //the main content will be hadded depending from the get value of content. If there is no get value of content it will show the default home page
     
+
+
     //this will set the content request to index or the requested page
     if (!empty($_GET['content'])) {
       $contentRequest = $_GET['content'];
@@ -9,12 +11,38 @@
       $contentRequest = 'index';
     }
 
-    $wholePath = 'mvc/view/content/' . $contentRequest . '.php';
-    //here we check if the page exist.. if it doesnt, we forward to the 404 page.
-    if(is_file($wholePath)){
-        include_once($wholePath); 
-    }else{
+    //make sure the request does not include any special character like backslahes or dots that could be used by hacker
+    if(preg_match('^[a-zA-Z0-9]+$', $contentRequest)){
         include_once('mvc/view/content/404.php'); 
+    }else{
+        
+        //VIEW
+        //this set the php path
+        $viewPath = 'mvc/view/content/' . $contentRequest . '.php';
+
+        //here we check if the view exist.. if it doesnt, we forward to the 404 page.
+        $viewPathExist = is_file($viewPath);
+        if($viewPathExist){
+            include_once($viewPath); 
+        }else{
+            include_once('mvc/view/content/404.php'); 
+        }
+
+        //we jsut load the following if the view path exists
+        if($viewPathExist){
+            //JS CONTROLLER
+            $controllerJsPath = 'mvc/controller/js/' . $contentRequest . '.js';
+
+            if(is_file($controllerJsPath)){
+                include_once($controllerJsPath); 
+            }
+
+            //PHP CONTROLLER
+            $controllerPhpPath = 'mvc/controller/php/' . $contentRequest . '.php';
+            if(is_file($controllerPhpPath)){
+                include_once($controllerJsPath); 
+            }
+        }
     }
 ?>
 <?php include_once('mvc/view/shared/footer.php'); ?>
